2da9f20e725c64f5222de3eec1f0f64c600471d5
Sashea <ghosteye519@gmail.com>
2023-05-09 19:15:22 +0200

    Cookkiiess!

--- passport-config.js
+++ passport-config.js
@@ -1,9 +1,9 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
-function initialize(passport, getUserByName, getUserById) {
-    const authUser = async (name, password, done) => {
-        const user = getUserByName(name)
+function initialize(passport, getUserByEmail, getUserById) {
+    const authUser = async (email, password, done) => {
+        const user = getUserByEmail(email)
         if (user == null) {
             return done(null, false, { mesasge: 'No user found'})
         }
@@ -19,9 +19,20 @@ function initialize(passport, getUserByName, getUserById) {
         }
     }
 
-    passport.use(new LocalStrategy({usernameField: 'name'}, authUser))
-    passport.serializeUser((user, done) => done(null, user.id))
-    passport.deserializeUser((id, done) => { return done(null, getUserById(id))})
-}
+    passport.use(new LocalStrategy({usernameField: 'email'}, authUser));
 
+    passport.serializeUser(function( user, done) {
+        process.nextTick(function() {
+            return done(null, {
+                email: user.email,
+                first_name: user.first_name
+            });
+        });
+    });
+    passport.deserializeUser(function(user, done) {
+        process.nextTick(function() {
+            return done(null, user);
+        })
+    })
+}
 module.exports = initialize
