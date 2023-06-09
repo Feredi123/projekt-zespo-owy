86fefcb7f43d01dbbc0d6f0de826ed59f6fe1f1f
Feredi123 <friedekmateusz@gmail.com>
2023-06-09 12:33:50 +0200

    poprawa growth

--- controllers/growth.js
+++ controllers/growth.js
@@ -83,65 +83,65 @@ async function postGrowthSkill(req, res) {
 
   async function getGrowthRaport(req, res) {
     try {
-      const [result] = await pool.query('SELECT s.name AS skill_name, e.first_name, e.second_name, DATE_FORMAT(g.start_date, "%d-%m-%Y") AS start_date, DATE_FORMAT(g.end_date, "%d-%m-%Y") AS end_date FROM growth AS g JOIN employees AS e ON e.employee_id = g.employees_employee_id JOIN skills AS s ON s.skills_id = g.skills_skill_id;');
-  
-      const aggregatedData = {};
-      for (const row of result) {
-        const key = row.skill_name;
-        if (!aggregatedData[key]) {
-          aggregatedData[key] = [];
-        }
-        aggregatedData[key].push(row);
-      }
-  
-      const doc = new PDFDocument();
-  
-      let isFirstPage = true;
-      for (const key in aggregatedData) {
-        if (aggregatedData.hasOwnProperty(key)) {
-          // Add a page for the skill
-          if (!isFirstPage) {
-            doc.addPage();
-          } else {
-            isFirstPage = false;
-          }
-  
-          // Set PDF section properties and content based on the skill's data
-          doc.fontSize(18).text(`Skill: ${key}`);
-          doc.moveDown();
-  
-          // Create table headers
-          // Create table headers
-          doc.font('Helvetica-Bold').fontSize(12);
-          const y = doc.y;
-          doc.text('Employee', 50, y);
-          doc.text('Start Date', 200, y);
-          doc.text('End Date', 300, y);
+        const [result] = await pool.query('SELECT s.name AS skill_name, e.first_name, e.second_name, DATE_FORMAT(g.start_date, "%d-%m-%Y") AS start_date, DATE_FORMAT(g.end_date, "%d-%m-%Y") AS end_date, g.level FROM growth AS g JOIN employees AS e ON e.employee_id = g.employees_employee_id JOIN skills AS s ON s.skills_id = g.skills_skill_id; ');
 
-  
-          // Display employees, start date, and end date in a table format
-          doc.font('Helvetica').fontSize(12);
-          for (const rowData of aggregatedData[key]) {
-            doc.moveDown();
-            const y = doc.y;
-            doc.text(`${rowData.first_name} ${rowData.second_name}`, 50, y);
-            doc.text(rowData.start_date, 200, y);
-            doc.text(rowData.end_date, 300, y);
-          }
+        const aggregatedData = {};
+        for (const row of result) {
+            const key = row.skill_name;
+            if (!aggregatedData[key]) {
+                aggregatedData[key] = [];
+            }
+            aggregatedData[key].push(row);
         }
-      }
-  
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', 'inline; filename=generated.pdf');
-  
-      doc.pipe(res);
-      doc.end();
-  
+
+        const doc = new PDFDocument();
+
+        let isFirstPage = true;
+        for (const key in aggregatedData) {
+            if (aggregatedData.hasOwnProperty(key)) {
+                // Add a page for the skill
+                if (!isFirstPage) {
+                    doc.addPage();
+                } else {
+                    isFirstPage = false;
+                }
+
+                // Set PDF section properties and content based on the skill's data
+                doc.fontSize(18).text(`Skill: ${key}`);
+                doc.moveDown();
+
+                // Create table headers
+                doc.font('Helvetica-Bold').fontSize(12);
+                const y = doc.y;
+                doc.text('Employee', 50, y);
+                doc.text('Start Date', 200, y);
+                doc.text('End Date', 300, y);
+                doc.text('Target Level', 400, y);
+
+                // Display employees, start date, end date and target level in a table format
+                doc.font('Helvetica').fontSize(12);
+                for (const rowData of aggregatedData[key]) {
+                    doc.moveDown();
+                    const y = doc.y;
+                    doc.text(`${rowData.first_name} ${rowData.second_name}`, 50, y);
+                    doc.text(rowData.start_date, 200, y);
+                    doc.text(rowData.end_date, 300, y);
+                    doc.text(rowData.level.toString(), 400, y);
+                }
+            }
+        }
+
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', `inline; filename=Growth Report ${new Date().toLocaleDateString("en-GB")}.pdf`);
+
+        doc.pipe(res);
+        doc.end();
+
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
     }
-  }
+}
   
   
   
